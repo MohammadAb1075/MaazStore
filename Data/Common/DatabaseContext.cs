250f799e015e4737bcf98e2b11e1ff4446697d5e
MohammadAb1075 <mabdollah1375gmail.com>
2022-12-29 03:36:26 -0800

    .

---
+++ Data/Common/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Data.Config;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Common;
+
+public class DatabaseContext : DbContext
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public DatabaseContext
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        (DbContextOptions<DatabaseContext> options) : base(options: options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly
+            (assembly: typeof(ProductConfig).Assembly);
+
+        modelBuilder.ApplyConfiguration(new ProductConfig());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
