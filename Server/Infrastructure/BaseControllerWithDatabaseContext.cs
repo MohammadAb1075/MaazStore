c82e8d14e2e7e0a4ce22b2c1823082c99ee1d666
MohammadAb1075 <mabdollah1375gmail.com>
2022-12-29 03:36:56 -0800

    AddBaseControllerWithDBConnection

--- Server/Infrastructure/BaseControllerWithDatabaseContext.cs
+++ Server/Infrastructure/BaseControllerWithDatabaseContext.cs
@@ -1,14 +1,16 @@
-﻿namespace Infrastructure;
+﻿using Data.Common;
+
+namespace Infrastructure;
 
 public abstract class BaseControllerWithDatabaseContext : BaseController
 {
     public BaseControllerWithDatabaseContext
-        (Data.DatabaseContext databaseContext) : base()
+        (DatabaseContext databaseContext) : base()
     {
         DatabaseContext = databaseContext;
     }
 
-    protected Data.DatabaseContext DatabaseContext { get; }
+    protected DatabaseContext DatabaseContext { get; }
 
     protected async
         System.Threading.Tasks.Task DisposeDatabaseContextAsync()
