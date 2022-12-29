9419e7f4f1c327d3dfcc835548d6f7fe8e25b18f
MohammadAb1075 <mabdollah1375gmail.com>
2022-12-29 01:41:29 -0800

    .

--- Server/Infrastructure/BaseControllerWithDatabaseContext.cs
+++ Server/Infrastructure/BaseControllerWithDatabaseContext.cs
@@ -1,25 +1,22 @@
-﻿//namespace Infrastructure;
+﻿namespace Infrastructure;
 
-//public abstract class BasePageModelWithDatabaseContext : BasePageModel
-//{
-//	public BasePageModelWithDatabaseContext
-//		(Data.DatabaseContext databaseContext) : base()
-//	{
-//		DatabaseContext = databaseContext;
-//	}
+public abstract class BaseControllerWithDatabaseContext : BaseController
+{
+    public BaseControllerWithDatabaseContext
+        (Data.DatabaseContext databaseContext) : base()
+    {
+        DatabaseContext = databaseContext;
+    }
 
-//	protected Data.DatabaseContext DatabaseContext { get; }
+    protected Data.DatabaseContext DatabaseContext { get; }
 
-//	//protected readonly Data.DatabaseContext DatabaseContext;
+    protected async
+        System.Threading.Tasks.Task DisposeDatabaseContextAsync()
+    {
+        if (DatabaseContext != null)
+        {
+            await DatabaseContext.DisposeAsync();
 
-//	protected async
-//		System.Threading.Tasks.Task DisposeDatabaseContextAsync()
-//	{
-//		if (DatabaseContext != null)
-//		{
-//			await DatabaseContext.DisposeAsync();
-
-//			//DatabaseContext = null;
-//		}
-//	}
-//}
+        }
+    }
+}
