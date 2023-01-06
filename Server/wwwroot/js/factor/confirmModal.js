5a4b6df1b1ec90033aad003cf86e1902cfc3dffd
MohammadAb1075 <mabdollah1375gmail.com>
2023-01-06 13:10:51 -0800

    AddDeleteConfirmation

---
+++ Server/wwwroot/js/factor/confirmModal.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('table').on('click', 'button.deleteItem', function () {
+        $('.modal#confirmModal').modal('show')
+        var href = $('.modal#confirmModal').find('a#deleteBtn').attr('href')
+        $('.modal#confirmModal').find('a#deleteBtn').attr('href', href + '/' + $(this).attr('linkAction'))
+    })
+    $('.modal#confirmModal').find('a#deleteBtn').click(function () {
+        $('.modal#confirmModal').modal('hide')
+    })
+})
