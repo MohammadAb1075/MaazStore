d065d6f87480d3888131ef78220bd4af8f04fe82
MohammadAb1075 <mabdollah1375gmail.com>
2023-01-06 11:54:59 -0800

    DebugFactorRowError

--- Server/wwwroot/js/factor/manageTable.js
+++ Server/wwwroot/js/factor/manageTable.js
@@ -1,10 +1,9 @@
-﻿var table;
+﻿var table = $('#factorRowTable');
 //var rowTable
 var rowOuterHtml = ''
 $(function () {
 
     function AddItem(btn) {
-        table = $('#factorRowTable');
         //rowTable = table.find('tbody tr').last()
         //rowOuterHtml = rowTable.get(0).outerHTML;
         var lastRow = table.find('tbody tr').last();
@@ -30,38 +29,6 @@ $(function () {
         rebindvalidators();
     }
 
-
-    //function AddItem(btn) {
-    //    var table;
-    //    table = document.getElementById('factorRowTable');
-    //    var rows = table.getElementsByTagName('tr');
-    //    var rowOuterHtml = rows[rows.length - 1].outerHTML;
-
-    //    var lastrowIdx = rows.length - 2;
-
-    //    var nextrowIdx = eval(lastrowIdx) + 1;
-
-    //    rowOuterHtml = rowOuterHtml.replaceAll('_' + lastrowIdx + '_', '_' + nextrowIdx + '_');
-    //    rowOuterHtml = rowOuterHtml.replaceAll('[' + lastrowIdx + ']', '[' + nextrowIdx + ']');
-    //    rowOuterHtml = rowOuterHtml.replaceAll('-' + lastrowIdx, '-' + nextrowIdx);
-
-    //    var newRow = table.insertRow();
-    //    newRow.innerHTML = rowOuterHtml;
-
-    //    var x = document.getElementsByTagName("INPUT");
-
-    //    for (var cnt = 0; cnt < x.length; cnt++) {
-    //        if (x[cnt].type == "text" && x[cnt].id.indexOf('_' + nextrowIdx + '_') > 0) {
-    //            if (x[cnt].id.indexOf('Unit') == 0)
-    //                x[cnt].value = '';
-    //        }
-    //        else if (x[cnt].type == "number" && x[cnt].id.indexOf('_' + nextrowIdx + '_') > 0)
-    //            x[cnt].value = 0;
-    //    }
-
-    //    rebindvalidators();
-    //}
-
     function rebindvalidators() {
 
         var $form = $("#factorForm");
@@ -77,7 +44,8 @@ $(function () {
 
     function DeleteItem(btn) {
         if (table.find('tbody tr').length > 1) {
-            btn.parents('tr').remove()
+            $(this).parents('table').find('.IsDeleted').val('1')
+            btn.parents('tr').hide()
         }
         calcTotal(table);
     }
